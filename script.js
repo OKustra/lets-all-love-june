65875893ff8186ac9e34867b953733b8d7282c2c
Oliver Kustra <34067198+OKustra@users.noreply.github.com>
2025-06-21 01:13:44 -0700

    Adds Map Coordinate Display
    A coordinate display can now be seen hovering near the mouse. These coordinates are equal to the map's SVG coordinates. This will help with setting coordinates for future message dots.
    
    This feature can be disabled within script.js by setting coordDisplayEnabled to false.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const messageMap = document.getElementById("message-map");
 const messageDotList = document.querySelectorAll(".message-dot");
+const coordDisplayEnabled = true;
+
+// Get messageMap's coordinates under the mouse cursor
+if (coordDisplayEnabled) {
+    // Creates a div in body for displaying coordinates
+    const coordDisplay = document.createElement("div");
+    coordDisplay.setAttribute("id", "coord-display");
+    coordDisplay.textContent = "x: 0, y: 0";
+    document.body.appendChild(coordDisplay);
+    // Add event listeners for mouse movement and update position
+    messageMap.addEventListener("mousemove", (event) => {
+        // Find mouse positions
+        const mouseScreenPos = new DOMPoint(event.clientX, event.clientY);
+        const mouseMapPos = mouseScreenPos.matrixTransform(messageMap.getScreenCTM().inverse());
+        // Update coordinate display
+        coordDisplay.textContent = `x: ${Math.round(mouseMapPos.x)}, y: ${Math.round(mouseMapPos.y)}`;
+        coordDisplay.style.left = event.pageX + 10 + 'px';
+        coordDisplay.style.top = event.pageY + 10 + 'px';
+    });
+}
 
 // Adds event listener to all message dots to display message on click
 messageDotList.forEach(messageDot => {
