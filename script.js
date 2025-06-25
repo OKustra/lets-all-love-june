b0441085f76fafc31caa8883e270433f5162e0c1
Oliver Kustra <34067198+OKustra@users.noreply.github.com>
2025-06-24 17:45:22 -0700

    Refactor message and music tab structure, add song collection
    Renames IDs and classes for clarity. Adds music collection functionality. Music tab is now hidden until a song is collected. Adds functionality to message that indicates when songs are collected. Reorganizes some selectors in CSS.

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-import { messages } from "./messageData.js";
+import { messages } from "./data/messageData.js";
+import { songIdToName } from "./data/songData.js";
 
 const messageMap = document.getElementById("message-map");
-const messageContent = document.getElementById("message-pane");
+const messageContent = document.getElementById("message-content");
+const musicTab = document.getElementById("music-tab");
+const trackList = document.getElementById("track-list");
 const coordDisplayEnabled = false;
 
 // Displays a dot's message in the message panel
 function displayMessage(messageDot) {
+    let compiledMessage = ""
+
     const friendName = messageDot.dataset.friendName;
-    const message = messageDot.dataset.message;
-    messageContent.innerHTML = `<strong>${friendName}</strong><br><br>${message.replace(/\n/g, "<br>")}`;
+    compiledMessage += `<strong>${friendName}</strong>`;
+
+    const friendMessage = messageDot.dataset.message;
+    compiledMessage += `<br><br>${friendMessage.replace(/\n/g, "<br>")}`
+
+    // On first click, read songIds from dataset if they exist, then add each to a notification that songs have been collected
+    if (messageDot.dataset.previouslyClicked == "false") {
+        if (messageDot.dataset.songIds) {
+        compiledMessage += `<br><br><span id="recommended-songs">Music added to collection!`
+
+        const friendSongs = messageDot.dataset.songIds.split(","); // Transforms data into iterable array
+        friendSongs.forEach(songId => {
+            compiledMessage += `<br><i>${songIdToName[songId]}</i>`
+        });
+
+        compiledMessage += `</span>`
+        }
+    }
+
+    messageContent.innerHTML = compiledMessage;
 };
+
+// Helper function to add songs to music content
+function addSongs(songIds) {
+    musicTab.classList.remove("hidden");
+
+    songIds.forEach(id => {
+        const songItem = document.createElement("li");
+        songItem.textContent = songIdToName[id];
+
+        trackList.appendChild(songItem);
+    });
+}
 
 // Darken a hex color. Used for dot stroke color
 function darkenHexColor(hex, factor = 0.7) {
@@ -67,10 +102,19 @@ messages.forEach(msg => {
     dot.setAttribute("stroke", darkenHexColor(msg.dotColor));
     dot.setAttribute("data-friend-name", msg.friendName)
     dot.setAttribute("data-message", msg.message);
+    dot.setAttribute("data-previously-clicked", false); // Changed to true on first click
+    if (msg.songIds) {
+        dot.setAttribute("data-song-ids", msg.songIds);
+    }
 
     // Adds event listener to message dot to display message on click
     dot.addEventListener("click", () => {
         displayMessage(dot);
+        if ((dot.dataset.previouslyClicked == "false") && (dot.dataset.songIds)) {
+            const songList = dot.dataset.songIds.split(",");
+            addSongs(songList);
+        }
+        dot.dataset.previouslyClicked = true;
     });
 
     // Append the dot to the message map
@@ -82,9 +126,7 @@ document.querySelectorAll("#side-tabs .tab").forEach(tabBtn => {
     tabBtn.addEventListener("click", () => {
         // Remove active from all tabs and panes
         document.querySelectorAll("#side-tabs .tab").forEach(btn => btn.classList.remove("active"));
-        document.querySelectorAll("#tab-contents .tab-pane").forEach(pane => pane.classList.remove("active"));
-
-        console.log(tabBtn);
+        document.querySelectorAll("#tab-content-container .tab-content").forEach(pane => pane.classList.remove("active"));
 
         // Activate this tab and it's pane
         tabBtn.classList.add("active");
